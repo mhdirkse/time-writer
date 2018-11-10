e759122df227d93309e5a161f097a0be41be764d
martijn <mhdirkse@live.nl>
2018-11-10 16:40:45 +0100

    Fix GreetingController to greet the logged user.

--- server/time-writer-base/src/main/java/com/github/mhdirkse/timewriter/GreetingController.java
+++ server/time-writer-base/src/main/java/com/github/mhdirkse/timewriter/GreetingController.java
@@ -1,5 +1,6 @@
 package com.github.mhdirkse.timewriter;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,9 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class GreetingController {
     @GetMapping("/hello")
-    public ModelAndView greet() {
+    public ModelAndView greet(@AuthenticationPrincipal UserPrincipal user) {
         ModelAndView result = new ModelAndView("hello");
-        result.getModel().put("name", "Martijn");
+        result.getModel().put("name", user.getUsername());
         return result;
     }
 }
